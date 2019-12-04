003e92ec9c7b8e000d77a16896d05e63bdf5fdc0
Johnnyr81 <emailjohn81@gmail.com>
2019-12-04 15:28:05 -0600

    udpate

--- router/theRouter.js
+++ router/theRouter.js
@@ -14,7 +14,7 @@ router.get('/', (req, res) => {
     .catch(error => {
       console.log(error);
       res.status(500).json({
-        message: 'Error retrieving the stuff',
+        error: "The posts information could not be retrieved." 
       });
     });
   });
@@ -57,34 +57,57 @@ router.get('/:id/comments', (req, res) => {
     });
     });
 
-    
 //POST
 router.post('/', (req, res) => {
-TheData.insert(req.body)
-.then(stuff => {
-    res.status(201).json(stuff);
-})
-.catch(error => {
-    console.log(error);
-    res.status(500).json({
-    message: 'Error adding',
-    });
-});
-});
 
+const updateData = req.body;    
 
-//POST COMMENTS BY ID ***
-router.post('/:id/comments', (req, res) => {
-    TheData.insertComment(req.body)
+if (updateData.title && updateData.contents) {   
+
+    TheData.insert(req.body)
     .then(stuff => {
         res.status(201).json(stuff);
     })
     .catch(error => {
         console.log(error);
         res.status(500).json({
-        message: 'Error adding comment',
+        message: 'Error adding',
         });
     });
+    }
+    else
+    {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post."  });
+    }
+
+});
+
+//POST COMMENTS BY ID ***
+router.post('/:id/comments', (req, res) => { 
+
+const updateData = req.body;
+const id = req.params.id;
+
+if (!updateData.text) {
+    res.status(400).json({ errorMessage: "Please provide text for the comment."  });
+}     
+else {
+        TheData.insertComment(req.body)
+        .then(stuff => {
+            if (stuff) {
+            res.status(201).json(stuff);
+            }
+            else{
+            res.status(404).json({ message: "The post with the specified ID does not exist." });    
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({
+            message: "There was an error while saving the comment to the database"
+            });
+        });
+    }
 });
 
 
@@ -109,22 +132,31 @@ TheData.remove(req.params.id)
 
 //UPDATE POST
 router.put('/:id', (req, res) => {
+
 const updatedPost = req.body;
-TheData.update(req.params.id, updatedPost)
-.then(updated => {
-    if (updated) {
-    res.status(200).json(updated);
-    } else {
-    res.status(404).json({ message: 'ID not found' });
-    }
-})
-.catch(error => {
-    // log error to database
-    console.log(error);
-    res.status(500).json({
-    message: 'Error updating the post',
+
+if (updateData.title && updateData.contents) { 
+
+    TheData.update(req.params.id, updatedPost)
+    .then(updated => {
+        if (updated) {
+        res.status(200).json(updated);
+        } else {
+        res.status(404).json({ message: 'ID not found' });
+        }
+    })
+    .catch(error => {
+        // log error to database
+        console.log(error);
+        res.status(500).json({
+        message: 'Error updating the post',
+        });
     });
-});
+
+    }
+    else{
+        res.status(400).json({ errorMessage: "Please provide name and bio for the user."  });  
+    }
 });
   
 module.exports = router;
